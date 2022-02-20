147c230578d06e127692361424c3b16290159ef7
Anton <bazhko.a.l@gmail.com>
2022-02-20 09:51:35 -0800

    Fixed grammar mistakes in funcrion names

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-const GET_RUNDOM_NUMBER = (min, max) => {
+const GET_RANDOM_NUMBER = (min, max) => {
   const result = (min >= 0 && min < max) ? Math.floor(Math.random() * (max - min + 1) + min) : 'Введите корректный диапозон от 0 и выше';
   return result;
 };
 
-GET_RUNDOM_NUMBER(-70, 50);
+GET_RANDOM_NUMBER(-70, 50);
 
 
-const GET_RUNDOM_FLOAT_NUMBER = (min, max, float) => {
+const GET_RANDOM_FLOAT_NUMBER = (min, max, float) => {
   const result = (min >= 0 && min < max) ? Math.random() * (max - min + 1) + min : 'Введите корректный диапозон от 0 и выше';
   return result.toFixed(float);
 };
 
-GET_RUNDOM_FLOAT_NUMBER(20.3, 80.6, 40);
+GET_RANDOM_FLOAT_NUMBER(20.3, 80.6, 40);
 
 
