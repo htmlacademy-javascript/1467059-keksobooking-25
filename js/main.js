3cb70566dcd46701f3897876acb66aa897c6d849
Anton <bazhko.a.l@gmail.com>
2022-02-21 20:40:28 -0800

    remove unnecessary variable

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-const GET_RANDOM_NUMBER = (min, max) => {
-  const result = (min >= 0 && min < max) ? Math.floor(Math.random() * (max - min + 1) + min) : 'Введите корректный диапозон от 0 и выше';
-  return result;
-};
+const GET_RANDOM_NUMBER = (min, max) =>
+  Number.isInteger(min & max) && min >= 0 && min < max ? Math.floor(Math.random() * (max - min + 1) + min) : 'Введите корректный диапозон от 0 и выше';
 
 GET_RANDOM_NUMBER(-70, 50);
 
-
 const GET_RANDOM_FLOAT_NUMBER = (min, max, float) => {
-  const result = (min >= 0 && min < max) ? Math.random() * (max - min + 1) + min : 'Введите корректный диапозон от 0 и выше';
+  const result = (Number.isInteger(min & max) && min >= 0 && min < max) ? Math.random() * (max - min + 1) + min : 'Введите корректный диапозон от 0 и выше';
   return result.toFixed(float);
 };
 
