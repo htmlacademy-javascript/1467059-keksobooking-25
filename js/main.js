51270a9e7e2d5b4a288a733ce312fde9b2e6ff3e
Anton <bazhko.a.l@gmail.com>
2022-03-09 16:25:13 -0800

    Changed consts and functions names

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-const GET_RANDOM_NUMBER = (a, b) => {
+const getRandomNumber = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
-const GET_RANDOM_FLOAT_NUMBER = (a, b, digits = 1) => {
+const GetRandomFloatNumber = (a, b, digits = 1) => {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
   const result = Math.random() * (upper - lower) + lower;
   return +result.toFixed(digits);
 };
 
-const getRandomArrayElement = (elements) => elements[GET_RANDOM_NUMBER(0, elements.length - 1)];
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
 const getArray = (object) => {
   const maxLength = object.length;
-  const lengthOfArray = GET_RANDOM_NUMBER(1, maxLength);
+  const lengthOfArray = getRandomNumber(1, maxLength);
   const array = [];
 
   while (array.length < lengthOfArray) {
-    const indexOfElement = GET_RANDOM_NUMBER(0, maxLength - 1);
+    const indexOfElement = getRandomNumber(0, maxLength - 1);
     const element = object[indexOfElement];
 
     if (!array.includes(element)) {
@@ -31,7 +31,7 @@ const getArray = (object) => {
   return array;
 };
 
-const LEAD_ZERO = (num, size) => {
+const LeadZero = (num, size) => {
   let s = `${num  }`;
   while (s.length < size) {
     s = `0${  s}`;
@@ -75,23 +75,23 @@ const PHOTOS_LIST = [
 ];
 
 const newObject = () => {
-  const RANDOM_NUMBER = LEAD_ZERO(GET_RANDOM_NUMBER(1, 10), 2);
+  const RANDOM_NUMBER = LeadZero(getRandomNumber(1, 10), 2);
   const author = {
     avatar: `img/avatars/user${  RANDOM_NUMBER}.png`
   };
 
   const location = {
-    lat: GET_RANDOM_FLOAT_NUMBER(35.65000, 35.70000, 5),
-    lng: GET_RANDOM_FLOAT_NUMBER(139.70000, 139.80000, 5)
+    lat: GetRandomFloatNumber(35.65000, 35.70000, 5),
+    lng: GetRandomFloatNumber(139.70000, 139.80000, 5)
   };
 
   const offer = {
     title: 'Luxury apartment',
     address: `${location.lat  }, ${  location.lng}`,
-    price: GET_RANDOM_NUMBER(0, 10000),
+    price: getRandomNumber(0, 10000),
     type: getRandomArrayElement(HOUSES_TYPES),
-    rooms: GET_RANDOM_NUMBER(0, 10),
-    guests: GET_RANDOM_NUMBER(0, 20),
+    rooms: getRandomNumber(0, 10),
+    guests: getRandomNumber(0, 20),
     checkin: getRandomArrayElement(CHECKIN_TIME),
     checkout: getRandomArrayElement(CHECKOUT_TIME),
     description: 'Unique Luxury Apartment',
@@ -107,4 +107,5 @@ const newObject = () => {
   };
 };
 
-const randomObjects = Array.from({length: 10}, newObject);
+const RANDOM_OBJECTS = Array.from({length: 10}, newObject);
+
