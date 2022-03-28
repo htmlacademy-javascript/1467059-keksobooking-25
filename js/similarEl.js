c51ba0e3a9d4c93bd1c24b8f7145b7a5095d49c5
Anton <bazhko.a.l@gmail.com>
2022-03-27 22:06:29 -0700

    changed similarElement module

--- js/similarEl.js
+++ js/similarEl.js
@@ -1,53 +1,74 @@
 import {newObject} from '/js/data.js';
 
+const similarNotices = Array.from({length: 1}, newObject);
 const noticeWindow = document.querySelector('.map__canvas');
 const noticeCardTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
-const similarNotices = Array.from({length: 1}, newObject);
+const typesDictionary = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+};
 
-similarNotices.forEach(({author, location, offer}) => {
+similarNotices.forEach(({author, offer}) => {
   const noticeCardEl = noticeCardTemplate.cloneNode(true);
-  noticeCardEl.querySelector('.popup__title').textContent = offer.title;
-  noticeCardEl.querySelector('.popup__text--address').textContent = offer.address;
+  const title = noticeCardEl.querySelector('.popup__title');
+  const address = noticeCardEl.querySelector('.popup__text--address');
+  const apartmentType = noticeCardEl.querySelector('.popup__type');
+  const price = noticeCardEl.querySelector('.popup__text--price');
+  const capacity = noticeCardEl.querySelector('.popup__text--capacity');
+  const checkTime = noticeCardEl.querySelector('.popup__text--time');
+  const description = noticeCardEl.querySelector('.popup__description');
+  const avatar = noticeCardEl.querySelector('.popup__avatar');
 
+  title.textContent = offer.title;
+  address.textContent = offer.address;
+  apartmentType.textContent = typesDictionary[offer.type];
+  price.textContent = `${offer.price} ₽/ночь`;
+  capacity.textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  checkTime.textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  description.textContent = offer.description;
+  avatar.src = author.avatar;
 
+  const getPhotos = () => {
+    const photosContainer = noticeCardEl.querySelector('.popup__photos');
+    const photosFragment = document.createDocumentFragment();
 
+    for (let i = 0; i < offer.photos.length - 1; i++) {
+      const newElement = noticeCardEl.querySelector('.popup__photo').cloneNode(false);
+      newElement.src = offer.photos[i];
+      photosFragment.appendChild(newElement);
+    }
 
-  noticeCardEl.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-  noticeCardEl.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  noticeCardEl.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  noticeCardEl.querySelector('.popup__description').textContent = offer.description;
+    photosContainer.textContent = '';
+    photosContainer.appendChild(photosFragment);
+  };
 
-  const photosContainer = noticeCardEl.querySelector('.popup__photos');
-  const photosFragment = document.createDocumentFragment();
-
-  for (let i = 0; i < offer.photos.length - 1; i++) {
-    const newElement = noticeCardEl.querySelector('.popup__photo').cloneNode(false);
-    newElement.src = offer.photos[i];
-    photosFragment.appendChild(newElement);
-  }
-
-  photosContainer.textContent = '';
-  photosContainer.appendChild(photosFragment);
-
-  noticeCardEl.querySelector('.popup__avatar').src = author.avatar;
+  getPhotos();
 
   noticeWindow.appendChild(noticeCardEl);
 
-  const apartmentFeatures = offer.features;
-  const featuresContainer = document.querySelector('.popup__features');
-  const featuresList = featuresContainer.querySelectorAll('.popup__feature');
+  const getApartmentFeatures = () => {
+    const apartmentFeatures = offer.features;
+    const featuresContainer = document.querySelector('.popup__features');
+    const featuresList = featuresContainer.querySelectorAll('.popup__feature');
 
-  featuresList.forEach((featuresListItem) => {
-    const isInclude = apartmentFeatures.some(
-      (apartmentFeature) => featuresListItem.classList.contains(`popup__feature--${apartmentFeature}`),
-    );
+    featuresList.forEach((featuresListItem) => {
+      const isInclude = apartmentFeatures.some(
+        (apartmentFeature) => featuresListItem.classList.contains(`popup__feature--${apartmentFeature}`),
+      );
 
-    if(!isInclude) {
-      featuresListItem.remove();
-    }
-  });
+      if(!isInclude) {
+        featuresListItem.remove();
+      }
+    });
+  };
+
+  getApartmentFeatures ();
+
 });
 
